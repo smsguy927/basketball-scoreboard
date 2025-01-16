030516d52d5db7659d9168868ee53a2240d34528
smsguy927 <smsguy927@gmail.com>
2025-01-15 23:20:02 -0600

    Improved timer functionality.

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-const crownIcon = '👑'
-const possesionIcon = '🏈'
-const bonusIcon = '🎉'
 const gameState = {
     home: {
         score: 0,
@@ -20,7 +17,23 @@ const gameState = {
     totalTimeTs: 0,
     currentPeriod: 0,
     totalPeriods: 0,
-    running: false
+    timePerPeriodTs: 0,
+    running: false,
+    isHalftime: false,
+    hasHalftimeRan: false,
+    ended: false
+}
+
+const constants = {
+    leadIcon: '👑',
+    possIcon: '🏈',
+    bonusIcon: '🎉',
+    foulsIcon: '❌',
+    maxFouls: 3,
+    halfTimeTs: 300,
+    hurryUpStart: 600,
+    countdownStart: 100,
+    periodSepChar: '/'
 }
 
 
@@ -33,8 +46,9 @@ const homePossBonus = document.getElementById("home-poss-bonus-container")
 const awayPossBonus = document.getElementById("away-poss-bonus-container")
 const homeFoulsCount = document.getElementById("home-fouls-count")
 const awayFoulsCount = document.getElementById("away-fouls-count")
-const time = document.getElementById("time")
+const timerSpan = document.getElementById("timer-span")
 const periodCount = document.getElementById("period-count")
+const periodSep = document.getElementById("period-sep")
 const totalPeriods = document.getElementById("period-total")
 
 function pressNewGame() {
@@ -51,7 +65,7 @@ newGameForm.addEventListener('submit', startGame)
 
 function startGame(e) {
     e.preventDefault()
-    alert(this === e.target)
+
 
     // hide form
     this[1].parentNode.hidden = true
@@ -63,12 +77,20 @@ function startGame(e) {
     clearPossBonus()
     clearFouls()
     resetTimer()
-    setPossession()
 
+    // Set Initial Game State
+    setPossession()
     setTimeParts(this[1].value)
     renderTime()
-    gameState.totalPeriods = this[2].value
+    gameState.currentPeriod = 1
+    gameState.totalPeriods = parseInt(this[2].value)
     totalPeriods.innerText = gameState.totalPeriods
+    gameState.timePerPeriodTs = gameState.totalTimeTs
+
+    // Game Loop
+
+    runTimer()
+
 
 }
 
@@ -99,7 +121,7 @@ function clearFouls() {
 
 function resetTimer() {
     gameState.timeTs = 0
-    time.textContent = '00:00:00'
+    timerSpan.textContent = '00:00:00'
     gameState.currentPeriod = 0
     periodCount.innerText = gameState.currentPeriod
     gameState.totalPeriods = 0
@@ -112,28 +134,34 @@ function flipACoin() {
 
 function setPossession() {
     if (flipACoin()) {
-        homePossBonus.children[0].innerText = possesionIcon
+        homePossBonus.children[0].innerText = constants.possIcon
         gameState.home.poss = true
     } else {
-        awayPossBonus.children[0].innerText = possesionIcon
+        awayPossBonus.children[0].innerText = constants.possIcon
         gameState.away.poss = true
     }
 }
 
 function setTimeParts(timeStr) {
-
     gameState.timeHrs = parseInt(timeStr.substring(0, 2))
     gameState.timeMins = parseInt(timeStr.substring(3, 5))
     gameState.timeSecs = parseInt(timeStr.substring(6, 8))
     calcTimeTs()
-
+}
+function setTimePartsFromTs() {
+    let timeS = gameState.totalTimeTs / 10
+    gameState.timeHrs = Math.floor(timeS / 3600)
+    timeS -= gameState.timeHrs * 3600
+    gameState.timeMins = Math.floor(timeS / 60)
+    timeS -= gameState.timeMins * 60
+    gameState.timeSecs = timeS
 }
 
 function renderTime() {
     const timeArr = []
     if (gameState.totalTimeTs < 600) {
         let displaySecs = gameState.totalTimeTs / 10
-        if(displaySecs % 1 === 0){
+        if (displaySecs % 1 === 0) {
             displaySecs += ".0"
         }
         timeArr.push(displaySecs)
@@ -144,8 +172,18 @@ function renderTime() {
         timeArr.push(gameState.timeMins.toString().padStart(2, '0'))
         timeArr.push(gameState.timeSecs.toString().padStart(2, '0'))
     }
-    time.innerText = timeArr.join(':')
+    if (gameState.totalTimeTs < constants.countdownStart) {
+        timerSpan.classList.add('countdown')
+        timerSpan.classList.remove('hurry-up')
+    } else if (gameState.totalTimeTs < constants.hurryUpStart) {
+        timerSpan.classList.add('hurry-up')
+    } else {
+        timerSpan.classList.remove('countdown')
+        timerSpan.classList.remove('hurry-up')
+    }
+    timerSpan.innerText = timeArr.join(':')
 }
+
 
 function calcTimeTs() {
 
@@ -158,8 +196,61 @@ function calcTimeTs() {
 
 }
 
-function runTimer(timeTs) {
-    const interval = setInterval(() => {
+function subtractSecond() {
+    if (gameState.timeSecs === 0 && gameState.timeMins === 0 && gameState.timeHrs === 0) {
+        return;
+    }
+    if (gameState.timeSecs === 0 && gameState.timeMins === 0) {
+        gameState.timeHrs -= 1
+        gameState.timeMins = 59
+        gameState.timeSecs = 59
+    } else if (gameState.timeSecs === 0) {
+        gameState.timeMins -= 1
+        gameState.timeSecs = 59
+    } else {
+        gameState.timeSecs -= 1
+    }
+}
 
+function advancePeriod() {
+    if (gameState.currentPeriod >= gameState.totalPeriods / 2 && !gameState.hasHalftimeRan) {
+        gameState.isHalftime = true
+        gameState.hasHalftimeRan = true
+        gameState.running = false
+        periodCount.innerText = "HALF"
+        periodSep.innerText = ""
+        totalPeriods.innerText = "TIME"
+        gameState.totalTimeTs = constants.halfTimeTs
+    } else {
+        gameState.currentPeriod += 1
+        periodCount.innerText = gameState.currentPeriod
+        periodSep.innerText = constants.periodSepChar
+        totalPeriods.innerText = gameState.totalPeriods
+        gameState.totalTimeTs = gameState.timePerPeriodTs
+        gameState.running = true
+        gameState.isHalftime = false
+    }
+    setTimePartsFromTs()
+}
+
+function runTimer() {
+    const interval = setInterval(() => {
+        // TODO: Add Pause and Reset Timer Functionality
+        if (gameState.totalTimeTs > 0) {
+            gameState.totalTimeTs -= 1
+            if (gameState.totalTimeTs % 10 === 9) {
+                subtractSecond()
+            }
+
+            renderTime()
+        } else if (gameState.currentPeriod < gameState.totalPeriods) {
+            advancePeriod()
+
+        } else {
+            clearInterval(interval)
+            gameState.running = false
+            gameState.ended = true
+            alert('Game Over')
+        }
     }, 100)
 }
